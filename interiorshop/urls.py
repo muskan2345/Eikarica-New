4a7333e310a5240115fbcfeca07e4b7e74de1a99
Hridyanshu <hridyanshusharma@gmail.com>
2021-07-30 02:25:37 +0530

    minor change

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -17,6 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -27,6 +30,7 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('', include('apps.product.urls')),
     path('accounts/',include('allauth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #test change by Hridyanshu
